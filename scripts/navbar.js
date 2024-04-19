74e7907f183f95f7efe446dbf3ba8b3a153fbee5
Paul McPhillips <paul@mcphillips.cc>
2024-04-19 10:58:53 +0100

    Added styling to make the links blend in and stand out better

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,10 +1,11 @@
-// Highlight the active page in the navigation bar
+/* Get the navbar item that's been selected */
 const navbarLinks = document.querySelectorAll('.navbar li a');
+/* For each item in the list add en event listener */
 navbarLinks.forEach(link => {
     link.addEventListener('click', () => {
-        // Remove 'active' class from all links
+        /* Remove 'active' class from all links */
         navbarLinks.forEach(l => l.classList.remove('active'));
-        // Add 'active' class to the clicked link
+        /* Add 'active' class to the clicked link */
         link.classList.add('active');
     });
 });
--- scripts/popout.js
+++ scripts/popout.js
@@ -1,15 +1,22 @@
+/* Get all images */
 const thumbnails = document.querySelectorAll('.imgitem img');
+/* Get the modal */
 const modal = document.getElementById('myModal');
+/* Get the image for the modal */
 const modalImage = document.getElementById('modalImage');
 
+/* For each image add an event listener */
 thumbnails.forEach(thumb => {
     thumb.addEventListener('click', (e) => {
+        /* Prevent the href call from opening the image in a new page */
         e.preventDefault();
+        /* Display the modal */
         modal.style.display = 'block';
         modalImage.src = thumb.parentNode.href;
     });
 });
 
+/* Hide the modal by changing its display back to none */
 function closeModal() {
     modal.style.display = 'none';
 }
